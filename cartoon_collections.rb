e3966eb03877a082b297888ffe49695316c462c3
friendly-machine-4891 <liamhanafee@gmail.com>
2020-09-03 15:22:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   
   planeteer_calls.collect do |phrase|
-    phrase.capitalize
+    phrase.capitalize << "!"
   end
 end
 
